028a352714083a880f137f9b9c3ad53775b13516
于小平 <yxp626@163.com>
2021-05-31 21:52:10 +0800

    1.增加日志aop,统一日志输出。 2.logback增加打印日志跟踪号。

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -6,6 +6,8 @@ import com.course.server.dto.ResponseDto;
 import com.course.server.exception.ValidatorException;
 import com.course.server.service.ChapterService;
 import com.course.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -20,8 +22,12 @@ import javax.annotation.Resource;
 @RequestMapping("/admin/chapter")
 public class ChapterController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+    public static final String BUSINESS_NAME = "大章";
+
     @Resource
     private ChapterService chapterService;
+
 
     @PostMapping(value="/list",produces = "application/json;charset=UTF-8")
     public ResponseDto list(@RequestBody PageDto pageDto){

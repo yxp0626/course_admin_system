06a4e5c892438edb16607733d6798d1bf63be5d5
于小平 <yxp626@163.com>
2021-05-31 20:27:29 +0800

    1.制作Loading组件，内部用jquery blockui插件实现

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -108,10 +108,12 @@ export default {
 
     list(page){
       let _this=this;
+      Loading.show();
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
         page: page,
         size: _this.$refs.pagination.size,
       }).then((response)=>{
+        Loading.hide();
         console.log("查询大章列表结果:",response);
         let resp = response.data;
         _this.chapters = resp.content.list;
@@ -121,8 +123,10 @@ export default {
 
     save(page){
       let _this=this;
+      Loading.show();
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
       _this.chapter).then((response)=>{
+        Loading.hide();
         console.log("保存大章列表结果:",response);
         let resp = response.data;
         if(resp.success){
@@ -145,7 +149,9 @@ export default {
         confirmButtonText: '确认!'
       }).then((result) => {
         if (result.isConfirmed) {
+          Loading.show();
           _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
+            Loading.hide();
             console.log("删除大章列表结果:",response);
             let resp = response.data;
             if(resp.success){

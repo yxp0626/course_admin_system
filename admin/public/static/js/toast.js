fef0349c96c3cbfe36742e395c1756dad0d09dc5
于小平 <yxp626@163.com>
2021-05-31 20:13:59 +0800

    1.制作toast组件，内部用sweetalert2实现。

---
+++ admin/public/static/js/toast.js
@@ -0,0 +1,36 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
+toast = {
+    success: function (message){
+        Toast.fire({
+            icon: 'success',
+            title: message
+        });
+    },
+
+    error: function (message){
+        Toast.fire({
+            icon: 'error',
+            title: message
+        });
+    },
+
+    success: function (message){
+        Toast.fire({
+            icon: 'warning',
+            title: message
+        });
+    },
+
+
+}

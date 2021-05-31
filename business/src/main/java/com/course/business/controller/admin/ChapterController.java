892d9c9cc21704573a7bd84a5b70c6a0dddf20c2
于小平 <yxp626@163.com>
2021-05-31 10:35:53 +0800

    1.集成分页插件pagehelper，注意页码从1开始

---
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -0,0 +1,30 @@
+package com.course.business.controller.admin;
+
+import com.course.server.domain.Chapter;
+import com.course.server.dto.ChapterDto;
+import com.course.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author Xiaoping Yu
+ * @date 2021/5/28 - 9:55
+ */
+
+@RestController
+@RequestMapping("/admin/chapter")
+public class ChapterController {
+
+    @Resource
+    private ChapterService chapterService;
+
+    @RequestMapping(value="/list",produces = "application/json;charset=UTF-8")
+    public List<ChapterDto> list(){
+
+        return chapterService.list();
+
+    }
+}
---
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -0,0 +1,39 @@
+package com.course.server.service;
+
+import com.course.server.domain.Chapter;
+import com.course.server.domain.ChapterExample;
+import com.course.server.dto.ChapterDto;
+import com.course.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Xiaoping Yu
+ * @date 2021/5/28 - 14:38
+ */
+@Service
+public class ChapterService {
+
+    @Resource
+    private ChapterMapper chapterMapper;
+
+    public List<ChapterDto> list(){
+        /*插件分页语句规则:调用startPage方法之后，执行的第一个select语句会进行分页。执行分页查询功能至少需要两条sql，一条是查询总记录数，一条是查当前页的记录。*/
+        PageHelper.startPage(2,1);
+        ChapterExample chapterExample = new ChapterExample();
+        List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
+        List<ChapterDto> chapterDtoList = new ArrayList<>();
+        for (int i = 0, l = chapterList.size(); i < l; i++) {
+            Chapter chapter = chapterList.get(i);
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter,chapterDto);
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
+    }
+}

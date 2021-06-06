0b799b8ecc5bcfbcd4dc74701046a8c6e1cfdad7
于小平 <yxp626@163.com>
2021-06-06 19:43:42 +0800

    1.课程管理增加了自定义排序。

--- server/src/main/java/com/course/server/mapper/my/MyCourseMapper.java
+++ server/src/main/java/com/course/server/mapper/my/MyCourseMapper.java
@@ -1,5 +1,6 @@
 package com.course.server.mapper.my;
 
+import com.course.server.dto.SortDto;
 import org.apache.ibatis.annotations.Param;
 
 /**
@@ -9,4 +10,12 @@ import org.apache.ibatis.annotations.Param;
 public interface MyCourseMapper {
 
     int updateTime(@Param("courseId") String courseId);
+
+    int updateSort(SortDto sortDto);
+
+    int moveSortsBackward(SortDto sortDto);
+
+    int moveSortsForward(SortDto sortDto);
+
+
 }

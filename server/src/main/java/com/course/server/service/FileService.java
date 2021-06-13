cd1bbb234581310309b000de5ebcab586c911fc9
于小平 <yxp626@163.com>
2021-06-13 10:37:43 +0800

    1.大文件断点续传与极速秒传：根据数据库中是否有文件的key来新增或修改文件记录，在前端中修改分片索引从1开始。

--- server/src/main/java/com/course/server/service/FileService.java
+++ server/src/main/java/com/course/server/service/FileService.java
@@ -9,67 +9,80 @@ import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Date;
+import java.util.List;
 
-/**
- * @author Xiaoping Yu
- * @date 2021/5/28 - 14:38
- */
 @Service
 public class FileService {
 
     @Resource
     private FileMapper fileMapper;
-//列表查询
-    public void list(PageDto pageDto){
-        /*插件分页语句规则:调用startPage方法之后，执行的第一个select语句会进行分页。执行分页查询功能至少需要两条sql，一条是查询总记录数，一条是查当前页的记录。*/
-        //当传入的分页参数不合法时，比如0,0时，程序不会报错，而是查全部记录，分页不生效。
-        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         FileExample fileExample = new FileExample();
         List<File> fileList = fileMapper.selectByExample(fileExample);
         PageInfo<File> pageInfo = new PageInfo<>(fileList);
         pageDto.setTotal(pageInfo.getTotal());
-        List<FileDto> fileDtoList = new ArrayList<>();
-        for (int i = 0, l = fileList.size(); i < l; i++) {
-            File file = fileList.get(i);
-            FileDto fileDto = new FileDto();
-            BeanUtils.copyProperties(file,fileDto);
-            fileDtoList.add(fileDto);
-        }
-        pageDto.setList(fileList);
+        List<FileDto> fileDtoList = CopyUtil.copyList(fileList, FileDto.class);
+        pageDto.setList(fileDtoList);
     }
-//保存操作，id有值的时候更新，无值的时候新增。
-    public void save(FileDto fileDto){
-        File file = CopyUtil.copy(fileDto,File.class);
-        if (StringUtils.isEmpty(fileDto.getId())){
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(FileDto fileDto) {
+        File file = CopyUtil.copy(fileDto, File.class);
+        File fileDb = selectByKey(fileDto.getKey());
+        if (fileDb == null) {
             this.insert(file);
-        }
-        else {
-            this.update(file);
+        } else {
+            fileDb.setShardIndex(fileDto.getShardIndex());
+            this.update(fileDb);
         }
     }
 
-    private void insert(File file){
-                Date now = new Date();
+    /**
+     * 新增
+     */
+    private void insert(File file) {
+        Date now = new Date();
         file.setCreatedAt(now);
         file.setUpdatedAt(now);
         file.setId(UuidUtil.getShortUuid());
         fileMapper.insert(file);
     }
 
-    private void update(File file){
-    file.setUpdatedAt(new Date());
+    /**
+     * 更新
+     */
+    private void update(File file) {
+        file.setUpdatedAt(new Date());
         fileMapper.updateByPrimaryKey(file);
     }
 
-    public void delete(String id){
+    /**
+     * 删除
+     */
+    public void delete(String id) {
         fileMapper.deleteByPrimaryKey(id);
+    }
+
+    public File selectByKey(String key) {
+        FileExample example = new FileExample();
+        example.createCriteria().andKeyEqualTo(key);
+        List<File> fileList = fileMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(fileList)) {
+            return null;
+        } else {
+            return fileList.get(0);
+        }
     }
 }

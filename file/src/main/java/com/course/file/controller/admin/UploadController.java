a064e6cca5e3a52b57668dbb47a933c43525fdb4
于小平 <yxp626@163.com>
2021-06-13 10:29:05 +0800

    1.大文件断点续传与极速秒传：使用文件在前端生成md5签名，作为文件的标识key，通过请求参数传递给后端。后端将此md5签名保存在数据库中，用作判断分片传输过程是否在传输同一个文件。

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -4,7 +4,6 @@ import com.course.server.dto.FileDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.enums.FileUseEnum;
 import com.course.server.service.FileService;
-import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -45,12 +44,12 @@ public class UploadController {
                               Integer size,
                               Integer shardIndex,
                               Integer shardSize,
-                              Integer shardTotal) throws IOException {
+                              Integer shardTotal,
+                              String key) throws IOException {
         LOG.info("上传文件开始");
 
         // 保存文件到本地
         FileUseEnum useEnum = FileUseEnum.getByCode(use);
-        String key = UuidUtil.getShortUuid();
 
         //如果文件夹不存在则创建
         String dir = useEnum.name().toLowerCase();

a21d0d8214c7abad6c6b941edc31cb712fa07dbe
于小平 <yxp626@163.com>
2021-06-13 09:15:27 +0800

    1.文件上传功能开发：增加课程内容文件管理，用于富文本框中插入图片或视频。

--- admin/public/static/js/tool.js
+++ admin/public/static/js/tool.js
@@ -14,7 +14,7 @@ Tool = {
      * 非空校验
      */
     isNotEmpty: function (obj) {
-        return !this.isEmpty(obj);
+        return !this.isEmpty();
     },
 
     /**
@@ -24,27 +24,6 @@ Tool = {
         return $.trim(str).length >= min && $.trim(str).length <= max;
     },
 
-    dateFormat: function (format, date) {
-        let result;
-        if (!date) {
-            date = new Date();
-        }
-        const option = {
-            "y+": date.getFullYear().toString(),        // 年
-            "M+": (date.getMonth() + 1).toString(),     // 月
-            "d+": date.getDate().toString(),            // 日
-            "h+": date.getHours().toString(),           // 时
-            "m+": date.getMinutes().toString(),         // 分
-            "s+": date.getSeconds().toString()          // 秒
-        };
-        for (let i in option) {
-            result = new RegExp("(" + i + ")").exec(format);
-            if (result) {
-                format = format.replace(result[1], (result[1].length == 1) ? (option[i]) : (option[i].padStart(result[1].length, "0")))
-            }
-        }
-        return format;
-    },
     /**
      * 时间格式化，date为空时取当前时间
      */
@@ -86,73 +65,5 @@ Tool = {
             }
         }
         return index;
-    },
-
-    /**
-     * 10进制转62进制
-     * @param number
-     * @returns {string}
-     * @private
-     */
-    _10to62: function (number) {
-        let chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ';
-        let radix = chars.length;
-        let arr = [];
-        do {
-            let mod = number % radix;
-            number = (number - mod) / radix;
-            arr.unshift(chars[mod]);
-        } while (number);
-        return arr.join('');
-    },
-
-    /**
-     * 保存登录用户信息
-     */
-    setLoginUser: function (loginUser) {
-        SessionStorage.set(SESSION_KEY_LOGIN_USER, loginUser);
-    },
-
-    /**
-     * 获取登录用户信息
-     */
-    getLoginUser: function () {
-        return SessionStorage.get(SESSION_KEY_LOGIN_USER) || {};
-    },
-
-    /**
-     * 随机生成[len]长度的[radix]进制数
-     * @param len
-     * @param radix 默认62
-     * @returns {string}
-     */
-    uuid: function (len, radix) {
-        let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-        let uuid = [];
-        radix = radix || chars.length;
-
-        for (let i = 0; i < len; i++) {
-            uuid[i] = chars[0 | Math.random() * radix];
-        }
-
-        return uuid.join('');
-    },
-
-    /**
-     * 查找是否有权限
-     * @param id 资源id
-     */
-    hasResource: function (id) {
-        let _this = this;
-        let resources = _this.getLoginUser().resources;
-        if (_this.isEmpty(resources)) {
-            return false;
-        }
-        for (let i = 0; i < resources.length; i++) {
-            if (id === resources[i].id) {
-                return true;
-            }
-        }
-        return false;
     }
 };

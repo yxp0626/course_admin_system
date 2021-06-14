d1368dcc1cf7f851eb13b2b46a447de9885a1684
于小平 <yxp626@163.com>
2021-06-14 16:17:42 +0800

    1.用户管理与登录：基本的登录功能开发，校验用户名和密码。利用用户名作为key在数据库中查找记录，不管是用户名不存在还是密码不存在，都抛出用户名或密码不存在的异常，使得恶意用户无法实施有针对性的试探。 前端输入的密码经过两次md5加密再同数据库中的值比较，判断密码是否一致。

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -27,17 +27,17 @@
                     <form>
                       <fieldset>
                         <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username"/>
-															<i class="ace-icon fa fa-user"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input v-model="user.loginName" type="text" class="form-control" placeholder="用户名"/>
+                            <i class="ace-icon fa fa-user"></i>
+                          </span>
                         </label>
 
                         <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password"/>
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
+                          <span class="block input-icon input-icon-right">
+                            <input v-model="user.password" type="password" class="form-control" placeholder="密码"/>
+                            <i class="ace-icon fa fa-lock"></i>
+                          </span>
                         </label>
 
                         <div class="space"></div>
@@ -59,128 +59,13 @@
                         <div class="space-4"></div>
                       </fieldset>
                     </form>
+
                   </div><!-- /.widget-main -->
 
                 </div><!-- /.widget-body -->
               </div><!-- /.login-box -->
-
-              <div id="forgot-box" class="forgot-box widget-box no-border">
-                <div class="widget-body">
-                  <div class="widget-main">
-                    <h4 class="header red lighter bigger">
-                      <i class="ace-icon fa fa-key"></i>
-                      Retrieve Password
-                    </h4>
-
-                    <div class="space-6"></div>
-                    <p>
-                      Enter your email and to receive instructions
-                    </p>
-
-                    <form>
-                      <fieldset>
-                        <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="email" class="form-control" placeholder="Email"/>
-															<i class="ace-icon fa fa-envelope"></i>
-														</span>
-                        </label>
-
-                        <div class="clearfix">
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-danger">
-                            <i class="ace-icon fa fa-lightbulb-o"></i>
-                            <span class="bigger-110">Send Me!</span>
-                          </button>
-                        </div>
-                      </fieldset>
-                    </form>
-                  </div><!-- /.widget-main -->
-
-                  <div class="toolbar center">
-                    <a href="#" data-target="#login-box" class="back-to-login-link">
-                      Back to login
-                      <i class="ace-icon fa fa-arrow-right"></i>
-                    </a>
-                  </div>
-                </div><!-- /.widget-body -->
-              </div><!-- /.forgot-box -->
-
-              <div id="signup-box" class="signup-box widget-box no-border">
-                <div class="widget-body">
-                  <div class="widget-main">
-                    <h4 class="header green lighter bigger">
-                      <i class="ace-icon fa fa-users blue"></i>
-                      New User Registration
-                    </h4>
-
-                    <div class="space-6"></div>
-                    <p> Enter your details to begin: </p>
-
-                    <form>
-                      <fieldset>
-                        <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="email" class="form-control" placeholder="Email"/>
-															<i class="ace-icon fa fa-envelope"></i>
-														</span>
-                        </label>
-
-                        <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username"/>
-															<i class="ace-icon fa fa-user"></i>
-														</span>
-                        </label>
-
-                        <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password"/>
-															<i class="ace-icon fa fa-lock"></i>
-														</span>
-                        </label>
-
-                        <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Repeat password"/>
-															<i class="ace-icon fa fa-retweet"></i>
-														</span>
-                        </label>
-
-                        <label class="block">
-                          <input type="checkbox" class="ace"/>
-                          <span class="lbl">
-															I accept the
-															<a href="#">User Agreement</a>
-														</span>
-                        </label>
-
-                        <div class="space-24"></div>
-
-                        <div class="clearfix">
-                          <button type="reset" class="width-30 pull-left btn btn-sm">
-                            <i class="ace-icon fa fa-refresh"></i>
-                            <span class="bigger-110">Reset</span>
-                          </button>
-
-                          <button type="button" class="width-65 pull-right btn btn-sm btn-success">
-                            <span class="bigger-110">Register</span>
-
-                            <i class="ace-icon fa fa-arrow-right icon-on-right"></i>
-                          </button>
-                        </div>
-                      </fieldset>
-                    </form>
-                  </div>
-
-                  <div class="toolbar center">
-                    <a href="#" data-target="#login-box" class="back-to-login-link">
-                      <i class="ace-icon fa fa-arrow-left"></i>
-                      Back to login
-                    </a>
-                  </div>
-                </div><!-- /.widget-body -->
-              </div><!-- /.signup-box -->
             </div><!-- /.position-relative -->
+
           </div>
         </div><!-- /.col -->
       </div><!-- /.row -->
@@ -189,19 +74,35 @@
 </template>
 
 <script>
-  export default {
-    name: 'login',
-    mounted: function (){
-        $('body').removeClass('class', 'no-skin');
-        $('body').attr('class', 'login-layout light-login');
-        //console.log("login");
-
-    },
-    methods: {
-      login (){
-        this.$router.push("/welcome")
-      }
+export default {
+  name: "login",
+  data: function() {
+    return {
+      user: {},
     }
-  }
-</script>
+  },
+  mounted: function() {
+    $("body").removeClass("no-skin");
+    $("body").attr("class", "login-layout light-login");
+    // console.log("login");
+  },
+  methods: {
+    login () {
+      let _this = this;
 
+      _this.user.password = hex_md5(_this.user.password + KEY);
+      Loading.show();
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
+        Loading.hide();
+        let resp = response.data;
+        if (resp.success) {
+          console.log(resp.content);
+          _this.$router.push("/welcome")
+        } else {
+          Toast.warning(resp.message)
+        }
+      });
+    },
+  }
+}
+</script>
--- server/src/main/java/com/course/server/exception/BusinessExceptionCode.java
+++ server/src/main/java/com/course/server/exception/BusinessExceptionCode.java
@@ -1,13 +1,9 @@
 package com.course.server.exception;
 
-/**
- * @author Xiaoping Yu
- * @date 2021/6/14 - 14:47
- */
-
 public enum BusinessExceptionCode {
 
     USER_LOGIN_NAME_EXIST("登录名已存在"),
+    LOGIN_ERROR("用户名或密码不存在"),
     ;
 
     private String desc;

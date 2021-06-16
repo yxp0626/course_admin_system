8ada8fa957858ae6047548ccac2660730a699ced
于小平 <yxp626@163.com>
2021-06-16 16:51:12 +0800

    1.用户管理与登录：集成Redis，图片验证码由存储在session中改为存储在Redis中。 使用的阿里云的Redis缓存，并没有使用本地的Redis缓存。只需要在maven中引入Redis的相关依赖即可。

--- system/src/main/java/com/course/system/controller/admin/UserController.java
+++ system/src/main/java/com/course/system/controller/admin/UserController.java
@@ -5,6 +5,7 @@ import com.course.server.service.UserService;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,9 @@ public class UserController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     /**
      * 列表查询
@@ -83,7 +87,9 @@ public class UserController {
         ResponseDto responseDto = new ResponseDto();
 
         // 根据验证码token去获取缓存中的验证码，和用户输入的验证码是否一致
-        String imageCode = (String) request.getSession().getAttribute(userDto.getImageCodeToken());
+        // String imageCode = (String) request.getSession().getAttribute(userDto.getImageCodeToken());
+        String imageCode = (String) redisTemplate.opsForValue().get(userDto.getImageCodeToken());
+        LOG.info("从redis中获取到的验证码：{}", imageCode);
         if (StringUtils.isEmpty(imageCode)) {
             responseDto.setSuccess(false);
             responseDto.setMessage("验证码已过期");

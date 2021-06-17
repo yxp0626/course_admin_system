a441d93ccbb03662c588aa3284aa5afcae017c12
于小平 <yxp626@163.com>
2021-06-17 16:17:55 +0800

    1.通用权限管理：资源树的保存：前端，通过在文本框中输入json格式的文本。后端接收到后，进行树结构的解析，解析完毕后存入数据库展示出来。

--- server/src/main/java/com/course/server/service/ResourceService.java
+++ server/src/main/java/com/course/server/service/ResourceService.java
@@ -1,69 +1,114 @@
 package com.course.server.service;
 
+import com.alibaba.fastjson.JSON;
 import com.course.server.domain.Resource;
 import com.course.server.domain.ResourceExample;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResourceDto;
 import com.course.server.mapper.ResourceMapper;
 import com.course.server.util.CopyUtil;
-import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.beans.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Xiaoping Yu
- * @date 2021/5/28 - 14:38
- */
 @Service
 public class ResourceService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ResourceService.class);
+
     @javax.annotation.Resource
     private ResourceMapper resourceMapper;
-//列表查询
-    public void list(PageDto pageDto){
-        /*插件分页语句规则:调用startPage方法之后，执行的第一个select语句会进行分页。执行分页查询功能至少需要两条sql，一条是查询总记录数，一条是查当前页的记录。*/
-        //当传入的分页参数不合法时，比如0,0时，程序不会报错，而是查全部记录，分页不生效。
-        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ResourceExample resourceExample = new ResourceExample();
         List<Resource> resourceList = resourceMapper.selectByExample(resourceExample);
         PageInfo<Resource> pageInfo = new PageInfo<>(resourceList);
         pageDto.setTotal(pageInfo.getTotal());
-        List<ResourceDto> resourceDtoList = new ArrayList<>();
-        for (int i = 0, l = resourceList.size(); i < l; i++) {
-            Resource resource = resourceList.get(i);
-            ResourceDto resourceDto = new ResourceDto();
-            BeanUtils.copyProperties(resource,resourceDto);
-            resourceDtoList.add(resourceDto);
-        }
-        pageDto.setList(resourceList);
+        List<ResourceDto> resourceDtoList = CopyUtil.copyList(resourceList, ResourceDto.class);
+        pageDto.setList(resourceDtoList);
     }
-//保存操作，id有值的时候更新，无值的时候新增。
-    public void save(ResourceDto resourceDto){
-        Resource resource = CopyUtil.copy(resourceDto,Resource.class);
-        if (StringUtils.isEmpty(resourceDto.getId())){
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(ResourceDto resourceDto) {
+        Resource resource = CopyUtil.copy(resourceDto, Resource.class);
+        if (StringUtils.isEmpty(resourceDto.getId())) {
             this.insert(resource);
-        }
-        else {
+        } else {
             this.update(resource);
         }
     }
 
-    private void insert(Resource resource){
-        resource.setId(UuidUtil.getShortUuid());
+    /**
+     * 新增，ID是自定义好的，不是自动生成的
+     */
+    private void insert(Resource resource) {
         resourceMapper.insert(resource);
     }
 
-    private void update(Resource resource){
+    /**
+     * 更新
+     */
+    private void update(Resource resource) {
         resourceMapper.updateByPrimaryKey(resource);
     }
 
-    public void delete(String id){
+    /**
+     * 删除
+     */
+    public void delete(String id) {
         resourceMapper.deleteByPrimaryKey(id);
+    }
+
+
+
+    /**
+     * 保存资源树
+     * @param json
+     */
+    @Transactional
+    public void saveJson(String json) {
+        List<ResourceDto> jsonList = JSON.parseArray(json, ResourceDto.class);
+        List<ResourceDto> list = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(jsonList)) {
+            for (ResourceDto d: jsonList) {
+                d.setParent("");
+                add(list, d);
+            }
+        }
+        LOG.info("共{}条", list.size());
+
+        resourceMapper.deleteByExample(null);
+        for (int i = 0; i < list.size(); i++) {
+            this.insert(CopyUtil.copy(list.get(i), Resource.class));
+        }
+    }
+
+    /**
+     * 递归，将树型结构的节点全部取出来，放到list
+     * @param list
+     * @param dto
+     */
+    public void add(List<ResourceDto> list, ResourceDto dto) {
+        list.add(dto);
+        if (!CollectionUtils.isEmpty(dto.getChildren())) {
+            for (ResourceDto d: dto.getChildren()) {
+                d.setParent(dto.getId());
+                add(list, d);
+            }
+        }
     }
 }
--- system/src/main/java/com/course/system/controller/admin/ResourceController.java
+++ system/src/main/java/com/course/system/controller/admin/ResourceController.java
@@ -1,9 +1,7 @@
 package com.course.system.controller.admin;
 
-import com.course.server.dto.ResourceDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
-import com.course.server.exception.ValidatorException;
 import com.course.server.service.ResourceService;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
@@ -11,12 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-
-
-/**
- * @author Xiaoping Yu
- * @date 2021/5/28 - 9:55
- */
 
 @RestController
 @RequestMapping("/admin/resource")
@@ -28,35 +20,37 @@ public class ResourceController {
     @Resource
     private ResourceService resourceService;
 
-//列表查询
-    @PostMapping(value="/list",produces = "application/json;charset=UTF-8")
-    public ResponseDto list(@RequestBody PageDto pageDto){
+    /**
+     * 列表查询
+     */
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
         ResponseDto responseDto = new ResponseDto();
         resourceService.list(pageDto);
         responseDto.setContent(pageDto);
         return responseDto;
-
     }
-//保存操作，id在数据库中含有时执行更新操作，id在数据库中不
-//含有时执行新增操作。
-    @PostMapping (value = "/save", produces = "application/json;charset=UTF-8")
-    public ResponseDto save(@RequestBody ResourceDto resourceDto) {
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody String jsonStr) {
         // 保存校验
-            ValidatorUtil.require(resourceDto.getName(), "名称");
-            ValidatorUtil.length(resourceDto.getName(), "名称", 1, 100);
-            ValidatorUtil.length(resourceDto.getPage(), "页面", 1, 50);
-            ValidatorUtil.length(resourceDto.getRequest(), "请求", 1, 200);
+        ValidatorUtil.require(jsonStr, "资源");
 
         ResponseDto responseDto = new ResponseDto();
-        resourceService.save(resourceDto);
-        responseDto.setContent(resourceDto);
+        resourceService.saveJson(jsonStr);
         return responseDto;
     }
 
-    @DeleteMapping(value = "/delete/{id}", produces = "application/json;charset=UTF-8")
-        public ResponseDto delete(@PathVariable String id){
-            ResponseDto responseDto = new ResponseDto();
-            resourceService.delete(id);
-            return responseDto;
+    /**
+     * 删除
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
+        resourceService.delete(id);
+        return responseDto;
     }
 }
